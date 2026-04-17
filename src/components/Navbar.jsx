2c8fd5735e6b983686b49f3044e67d5f40b981f3
Danijela Obradovic <danijelaobradovic10@gmail.com>
2026-04-17 17:43:19 +0200

    Minor styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
+import { FaFilm } from "react-icons/fa";
 
 function Navbar() {
   return (
     <nav>
-      <div className="navbar-brand">🎬 MoviesApp</div>
+      <div className="navbar-brand">
+        <FaFilm className="brand-icon" /> Cinescape
+      </div>
       <div className="navbar-links">
         <Link to="/">Home</Link>
         <Link to="/favorites">Favorites</Link>
